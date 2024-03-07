ba2d02e202b72fd8581fd93218ec6f11e90aa5cc
Tetsui Ohkubo <peryaudo@gmail.com>
2024-03-07 15:24:19 +0900

    Implement image augumentation

--- dataset.py
+++ dataset.py
@@ -1,31 +1,64 @@
 from datasets import load_from_disk, Features, Array3D
 import numpy as np
 from torch.utils.data import Dataset
+import albumentations as A
+from PIL import Image
+import torch
 
-def transforms(examples):
-    image = examples["image"]
-    hsv = np.array(image.resize((64,64)).convert("HSV"))
-    examples["hs"] = hsv[:,:,0:-1].transpose(2, 0, 1).astype(np.float32) / 255.0
-    examples["v"] =  hsv[:,:,-1][None,].astype(np.float32) / 255.0 - 0.5
-    return examples
 
-class HfDatasetWrapper(Dataset):
+def to_hsv(image):
+    hsv = np.array(image.convert("HSV"))
+    hs = hsv[:,:,0:-1].transpose(2, 0, 1).astype(np.float32) / 255.0
+    v =  hsv[:,:,-1][None,].astype(np.float32) / 255.0 - 0.5
+    return hs, v
+    
+
+class TrainDatasetWrapper(Dataset):
     def __init__(self, hf_dataset):
-        self.hf_dataset = hf_dataset
+        def map_func(examples):
+            examples["image"] = np.array(examples["image"].resize((96,96)))
+            return examples
+        self.hf_dataset = hf_dataset.map(map_func, batched=False, features=Features({
+            "image": Array3D(dtype="float32", shape=(96, 96, 3)),
+        })).with_format("numpy")
+        self.transforms = A.Compose([
+            A.RandomCrop(64, 64),
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.2),
+        ])
+
+    def __len__(self):
+        return len(self.hf_dataset)
+
+    def __getitem__(self, idx):
+        image = self.hf_dataset[idx]["image"]
+        image = self.transforms(image=image)["image"]
+        hs, v = to_hsv(Image.fromarray(image.astype(np.uint8)))
+        return torch.from_numpy(v), torch.from_numpy(hs)
+
+class ValDatasetWrapper(Dataset):
+    def __init__(self, hf_dataset):
+        def map_func(examples):
+            image = examples["image"]
+            hs, v = to_hsv(image.resize((64,64)))
+            examples["hs"] = hs
+            examples["v"] =  v
+            return examples
+        self.hf_dataset = hf_dataset.map(map_func, batched=False, features=Features({
+            "hs": Array3D(dtype="float32", shape=(2, 64, 64)),
+            "v": Array3D(dtype="float32", shape=(1, 64, 64)),
+        })).with_format("numpy")
 
     def __len__(self):
         return len(self.hf_dataset)
 
     def __getitem__(self, idx):
         item = self.hf_dataset[idx]
-        return (item['v'], item['hs'])
+        return torch.from_numpy(item["v"]), torch.from_numpy(item["hs"])
+
 
 def get_dataset():
-    features = Features({
-        "hs": Array3D(dtype="float32", shape=(2, 64, 64)),
-        "v": Array3D(dtype="float32", shape=(1, 64, 64)),
-    })
-
     dataset = load_from_disk("./flowers-102-split")
-    dataset = dataset.map(transforms, remove_columns=["image"], batched=False, features=features).with_format("torch")
-    return HfDatasetWrapper(dataset["train"]), HfDatasetWrapper(dataset["test"])
+    train_dataset = TrainDatasetWrapper(dataset["train"])
+    val_dataset = ValDatasetWrapper(dataset["test"])
+    return train_dataset, val_dataset
